41cd1fc5a7cfd13d4887074a737018991d3cb530
Vera <imbesom@mail.ru>
2022-09-25 18:06:18 +0300

    задача 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+void Задача1()
+{
+{
+Console.WriteLine("Напишите любое трёхзначное число");
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+int tens = number/10%10;
+int i = number%10;
+if((number>99&&number<=999)^(number>-999&&number<=-99))
+{
+ Console.WriteLine(tens + " - второе число в "+ number);
+}
+else
+{
+Console.WriteLine("Число не трехзначное");
+}
+}
+Задача1();
